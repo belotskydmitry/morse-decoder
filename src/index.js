bf358d3fde0f92c3a3845a30f77e7c3dfcedfa7b
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-08-06 14:51:10 +0300

    init: create task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  return expr.trim().split(' ').map( a => MORSE_TABLE[a] || ' ').join('').replace(/\s+/g, ' ');
 }
 
 module.exports = {
